4114e23c249b8777c46c31dbecfe15b45c40e3cf
Moser José <8234620+moser-jose@users.noreply.github.com>
2025-05-14 00:26:44 -0300

    update the code

--- src/universities/application/use-cases/get-university-by-slug.spec.ts
+++ src/universities/application/use-cases/get-university-by-slug.spec.ts
@@ -1,8 +1,8 @@
-
 import { Slug } from '../../enterprise/entities/value-objects/slug';
 import { GetUniversityBySlugUseCase } from './get-university-by-slug';
 import { InMemoryUniversitiesRepository } from 'test/repositories/in-universitary-repository';
-import { University } from '@/universities/enterprise/entities/university';
+import { makeUniversity } from 'test/factories/make-university';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 let inMemoryUniversitiesRepository: InMemoryUniversitiesRepository;
 let sut: GetUniversityBySlugUseCase;
@@ -12,56 +12,17 @@ describe('Get university by slug', () => {
     sut = new GetUniversityBySlugUseCase(inMemoryUniversitiesRepository);
   });
   it('should be able to get a university by slug', async () => {
-    const university = University.create({
-      name: 'University of Angola',
-      email: 'university@angola.edu.ao',
-      image: 'https://example.com/university.png',
-      website: 'https://university.edu.ao',
-      address: 'Angola',
-      description: 'University of Angola',
-      type: 'public',
-      founded: '1960',
-      phones: ['+244 922 222 222'],
-      social_media: {
-        facebook: 'https://facebook.com/university',
-        instagram: 'https://instagram.com/university',
-        twitter: 'https://twitter.com/university',
-        linkedin: 'https://linkedin.com/university'
-      },
-      awards: [],
-      management: [],
-      ranking: 1,
-      latitude: 1,
-      longitude: 1,
-      abbreviation: 'UAN',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    });
+    const university = makeUniversity({}, new UniqueEntityID('1'));
 
     inMemoryUniversitiesRepository.create(university);
 
     const result = await sut.execute({
-      slug: Slug.create('university-of-angola').value
+      slug: Slug.createFromText(university.name).value
     });
 
     expect(result.value).toMatchObject({
       university: {
-        name: university.name,
-        email: university.email,
-        image: university.image,
-        website: university.website,
-        address: university.address,
-        description: university.description,
-        type: university.type,
-        founded: university.founded,
-        phones: university.phones,
-        social_media: university.social_media,
-        awards: university.awards,
-        management: university.management,
-        ranking: university.ranking,
-        latitude: university.latitude,
-        longitude: university.longitude,
-        abbreviation: university.abbreviation,
+        name: university.name
       }
     });
   });
--- src/universities/application/use-cases/register-university.spec.ts
+++ src/universities/application/use-cases/register-university.spec.ts
@@ -1,6 +1,6 @@
-/* eslint-disable prettier/prettier */
 import { InMemoryUniversitiesRepository } from 'test/repositories/in-universitary-repository';
 import { RegisterUniversityUseCase } from './register-university';
+import { makeUniversity } from 'test/factories/make-university';
 
 let inMemoryUniversitiesRepository: InMemoryUniversitiesRepository;
 let sut: RegisterUniversityUseCase;
@@ -10,47 +10,8 @@ describe('Register university', () => {
     sut = new RegisterUniversityUseCase(inMemoryUniversitiesRepository);
   });
   it('it should be able to register a new university', async () => {
-    const result = await sut.execute({
-      name: 'University of Angola',
-      description: 'University of Angola',
-      image: 'University of Angola',
-      website: 'University of Angola',
-      address: 'University of Angola',
-      abbreviation: 'UA',
-      type: 'Public',
-      founded: '1954',
-      email: 'university@angola.edu.ao',
-      phones: ['+244 922 222 222'],
-      management: [
-        {
-          name: 'John Doe',
-          position: 'Rector',
-          email: 'john.doe@example.com'
-        },
-        {
-          name: 'Jane Doe',
-          position: 'Vice Rector',
-          email: 'jane.doe@example.com'
-        }
-      ],
-      social_media: {
-        facebook: 'https://www.facebook.com/universityofangola',
-        instagram: 'https://www.instagram.com/universityofangola',
-        twitter: 'https://www.twitter.com/universityofangola',
-        linkedin: 'https://www.linkedin.com/universityofangola'
-      },
-      latitude: 123.456,
-      longitude: 789.123,
-      ranking: 1,
-      awards: [
-        {
-          title: 'Award 1',
-          year: 2021,
-          organization: 'Organization 1'
-        }
-      ],
-      createdAt: new Date()
-    });
+    const university = makeUniversity();
+    const result = await sut.execute(university);
 
     expect(result.isRight()).toBe(true);
     expect(result.value).toEqual({
